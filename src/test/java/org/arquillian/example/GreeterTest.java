9559bdc17869b4bcfbe6cfa4e23ab649b3c318d4
Bodo Teichmann <bodo.teichmann@brandad-systemse.de>
2018-03-07 21:06:43 +0100

    changes to make it run in Eclipse

--- src/test/java/org/arquillian/example/GreeterTest.java
+++ src/test/java/org/arquillian/example/GreeterTest.java
@@ -1,26 +1,37 @@
 package org.arquillian.example;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import javax.inject.Inject;
 
 import org.jboss.arquillian.container.test.api.Deployment;
-
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
-import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+
 
 @RunWith(Arquillian.class)
 public class GreeterTest {
+	
 	 @Deployment
 	    public static JavaArchive createDeployment() {
+		 System.out.println("property: java.util.logging.manager=" + System.getProperty("java.util.logging.manager"));
+		 System.out.println("property: project.baseDir=" + System.getProperty("project.baseDir"));
+		 //need this in Eclipse, otherwise Test will not run inside Eclipse!	
+		 //set in Eclipse Run/Debug Configuration-> (x)=Arguments -> "vm Arguments" add this: 
+		 //-Djava.util.logging.manager=org.jboss.logmanager.LogManager  -Dproject.baseDir=${workspace_loc:myEclipseProjectName}
+		 //but when run from command line with gradle , these variables will be  set in the gradle.build file
+		
+		 assertEquals(System.getProperty("java.util.logging.manager"), "org.jboss.logmanager.LogManager");;
+		 assertNotNull(System.getProperty("project.baseDir"));
+				
 		 System.getProperties().entrySet().forEach(e -> {
-				//System.out.println("prop: " + e.getKey() + "=" + e.getValue());
+				//System.out.println("prop_all: " + e.getKey() + "=" + e.getValue());
 			});
 	        JavaArchive jar =  ShrinkWrap.create(JavaArchive.class)
 	            .addClasses(Greeter.class,PhraseBuilder.class)
